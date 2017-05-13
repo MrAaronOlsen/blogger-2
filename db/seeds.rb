fa7771ff19844808aa9c12c3bb4ee6bb69670612
Aaron Olsen <mraaronolsen@gmail.com>
2017-05-13 17:34:47 -0600

    Blog Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+body = "Helvetica venmo succulents, leggings next level blog pok pok tousled
+fap. Scenester ethical locavore, distillery direct trade chartreuse microdosing
+pop-up bicycle rights bespoke fashion axe biodiesel. Blue bottle irony plaid pug
+authentic, street art before they sold out kinfolk tumblr. Kombucha fanny pack
+ramps, meditation crucifix authentic deep v swag brooklyn marfa leggings kale
+chips occupy ennui XOXO. Air plant bicycle rights subway tile fanny pack.
+Drinking vinegar dreamcatcher gochujang kitsch, portland yr offal. Post-ironic
+mixtape fixie, master cleanse tumeric cornhole helvetica knausgaard occupy fanny
+pack.
+Fap beard raclette shabby chic kitsch green juice, unicorn shoreditch irony.
+Cray cronut literally, cliche neutra cold-pressed thundercats kitsch. Bespoke
+flannel DIY, organic umami selvage keffiyeh freegan fanny pack fam hot chicken
+lumbersexual kale chips bicycle rights cliche. Gastropub kogi hashtag chia,
+brunch cornhole hot chicken stumptown banjo. Irony sartorial put a bird on it
+vinyl biodiesel iceland photo booth tousled, pinterest pickled vice food truck
+DIY franzen. Banh mi ethical everyday carry, chartreuse viral meh echo park
+pitchfork tattooed small batch. Poutine salvia glossier mustache ennui fanny pack.
+Echo park bicycle rights blog, single-origin coffee freegan poke trust fund
+portland beard ugh lomo typewriter paleo bitters neutra. Gochujang raclette
+pabst man bun, normcore try-hard fingerstache YOLO neutra leggings af la croix.
+Gluten-free stumptown four loko vexillologist. Yr ethical la croix, cliche
+mustache coloring book retro glossier pour-over. Cliche swag organic, chia
+locavore man braid migas tofu cornhole. Everyday carry flannel semiotics waistcoat.
+Kinfolk polaroid celiac fashion axe fingerstache hashtag, pickled hoodie migas
+shabby chic slow-carb freegan la croix you probably haven't heard of them."
+
+Blog.create(title: 'Blog Post-ironic', body: body)
+Blog.create(title: 'Retro Bowling', body: body)
+Blog.create(title: 'Pink Blast Fungus', body: body)
+Blog.create(title: 'Star Gazing Mars', body: body)
