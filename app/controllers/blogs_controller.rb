57e8298969241b34c8bd87fd993d8719d9b57ae6
Aaron Olsen <mraaronolsen@gmail.com>
2017-05-13 17:56:59 -0600

    Show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,18 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show]
 
   def index
     @blogs = Blog.all
   end
-  
+
+  def show
+  end
+
+
 end
+
+private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
